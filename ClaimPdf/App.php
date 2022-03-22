9784db25ab0117b86db504dc3abc64481f4e8715
chinhle-pa <61530823+chinhle-pa@users.noreply.github.com>
2022-03-22 16:01:41 +0700

    start coding

--- ClaimPdf/App.php
+++ ClaimPdf/App.php
@@ -2,6 +2,9 @@
 
 namespace ClaimPdf;
 
+use FPDM;
+use mikehaertl\pdftk\Pdf;
+
 /**
  * Class App
  *
@@ -15,6 +18,11 @@ class App
      */
     private $config;
 
+    private $pdf;
+    private $fields;
+    private $lib;
+    private $libs = ['Pdf', 'FPDM'];
+
     /**
      * Sample constructor.
      *
@@ -23,6 +31,16 @@ class App
     public function __construct(Config $config)
     {
         $this->config = $config;
+        $pdf_config = $config->get('pdf');
+        $this->lib = $pdf_config['lib'];
+        if(!\in_array($this->lib, $this->libs)){
+            $this->lib = 'Pdf';
+        }
+        $this->fields = $pdf_config['fields'];
+        $this->pdf = new Pdf(dirname(__FILE__).$pdf_config['path'].$pdf_config['name']);
+        if($this->lib == 'FPDM'){
+            $this->pdf = new FPDM(dirname(__FILE__).$pdf_config['path'].$pdf_config['name']);
+        }
     }
 
     /**
@@ -35,6 +53,35 @@ class App
         $greeting = $this->config->get('greeting');
 
         return $greeting . ' ' . $name;
+    }
+    public function getInfo() {
+        return $this->pdf->Info();
+    }
+    public function pdfLoad($fields) {
+        $this->fields = array_merge($this->fields, $fields);
+        if($this->lib == 'FPDM') {
+            $this->pdf->Load($this->fields, true);
+        }
+    }
+    public function pdfOutput($fields) {
+        $this->pdfLoad($fields);
+        $result = $this->pdf->fillForm($this->fields)->needAppearances()->saveAs('filled.pdf');
+        // Always check for errors
+        if ($result === false) {
+            $error = $this->pdf->getError();
+            dd($error);
+        } else {
+            // $pdf = new Pdf('filled.pdf');
+            // $result = $pdf->generateFdfFile('/path/data.fdf');
+            // if ($result === false) {
+            //     $error = $pdf->getError();
+            // }
+
+        }
+        if($this->lib == 'FPDM'){
+            // $this->pdf->Merge();
+            // $this->pdf->Output();
+        }
     }
 
 }
